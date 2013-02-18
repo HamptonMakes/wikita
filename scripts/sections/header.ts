faf80e242f781a82c76543ef4deb1e56b6facd2e
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2013-02-18 12:32:22 -0800

    getting there

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -4,16 +4,38 @@ $("./head") {
   }
 }
 
+$$("li#n-shoplink") { remove() }
+
 $$("#mw-page-base, #mw-head-base") {
   remove()
 }
 $$("#p-personal") {
+  $("/html/body//div[@id='p-navigation']//div[@class='body']") { 
+
+    insert_top("ul", class: "personalNav")
+    insert_top("span", "Account", class: "navHeading")
+  }
   $$("li") {
     attribute("id") { remove() }
-    move_to("/html/body//div[@id='p-navigation']//div[@class='body']/ul", "top")
+    move_to("//ul[@class = 'personalNav']", "top")
   }
   remove()
 }
+
+$("/html/body//div[@id='p-navigation']//div[@class='body']") { 
+  insert_top("ul", class: "pageNav")
+  insert_top("span", "Page", class: "navHeading")
+}
+$$(".vectorTabs") {
+  $$(".selected") {
+    remove()
+  }
+  
+  $$("li") {
+    move_to("//ul[@class = 'pageNav']", "top")
+  }
+}
+
 $$("#mw-head") {
 
 }
